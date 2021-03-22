23182caa8b693df3d79b6c9550eab425bfb7e1ac
Lars Alexander Jakobsen <larsajakobsen@gmail.com>
2021-03-22 20:20:39 +0100

    Add test app

---
+++ src/app/test-app/src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form-container">
+<form>
+
+Email:
+<input type="email" id="email" name="email"/> <br>
+<br> <br>
+Password:
+<input type="Password" id="pass" name="pass"> <br>
+<br> <br>
+Re-type password:
+<input type="Password" id="repass" name="repass"> <br> <br>
+<input type="button" value="Submit"/>
+</form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.form-container input {
+  display: inline;
+}
+</style>
